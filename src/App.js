1724e0ff27f4408786d08c1b4782f85884d60950
ARWAIMORISH <morisarwai@gmail.com>
2020-05-02 19:50:37 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css';
+
+class App extends Component {
+
+state = {
+  todos: [
+    {
+      id: 1,
+      title: "Take out trash",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Dinna with wife",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Meeting with boss",
+      completed: false
+    }
+  ]
+}
+
+markComplete = (id) => {
+  console.log(id)
+} 
+
+  render(){
+    return (
+      <div className="App">
+
+        <Todos 
+        todos={this.state.todos} 
+        markComplete = {this.markComplete}
+        
+        />
+      </div>
+    );
+  }
+  }
+
+export default App;
